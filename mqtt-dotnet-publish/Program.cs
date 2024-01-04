b1566a69f3a02b99b3a9ff273d22cf52186a9a6e
Lucas Aguiar <lucas99.abreu@outlook.com>
2024-01-04 18:38:17 -0300

    first commit

---
+++ mqtt-dotnet-publish/Program.cs
@@ -0,0 +1,52 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+await Publish();
+return;
+
+async Task Publish()
+{
+    var factory = new MqttFactory();
+
+    using var client = factory.CreateMqttClient();
+    var clientOptions = new MqttClientOptionsBuilder()
+        .WithTcpServer("localhost")
+        .Build();
+
+    await client.ConnectAsync(clientOptions, CancellationToken.None);
+
+    var applicationMessage = new MqttApplicationMessageBuilder()
+        .WithTopic("samples/temperature/living_room")
+        .WithPayload("19.5")
+        .Build();
+
+    await client.PublishAsync(applicationMessage, CancellationToken.None);
+
+    await client.DisconnectAsync();
+
+    Console.WriteLine("MQTT application message is published.");
+}
+
+async Task PublishFile()
+{
+    var factory = new MqttFactory();
+
+    using var client = factory.CreateMqttClient();
+    var clientOptions = new MqttClientOptionsBuilder()
+        .WithTcpServer("localhost")
+        .Build();
+
+    await client.ConnectAsync(clientOptions, CancellationToken.None);
+
+    var payload = File.ReadAllBytes("/Volumes/External/www/mqtt-dotnet/mqtt-dotnet-publish/assets/definição_totem_Sinetram.docx");
+    var applicationMessage = new MqttApplicationMessageBuilder()
+        .WithTopic("samples/temperature/living_room/file")
+        .WithPayload(payload)
+        .Build();
+
+    await client.PublishAsync(applicationMessage, CancellationToken.None);
+
+    await client.DisconnectAsync();
+
+    Console.WriteLine("MQTT application message is published.");
+}
---
+++ mqtt-dotnet-subscribe/Program.cs
@@ -0,0 +1,73 @@
+﻿using mqtt_dotnet_library;
+using MQTTnet;
+using MQTTnet.Client;
+
+await Subscribe();
+return;
+
+async Task Subscribe()
+{
+    var mqttFactory = new MqttFactory();
+
+    using var mqttClient = mqttFactory.CreateMqttClient();
+    var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
+
+    mqttClient.ApplicationMessageReceivedAsync += e =>
+    {
+        Console.WriteLine("Received application message.");
+        e.DumpToConsole();
+
+        return Task.CompletedTask;
+    };
+
+    await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+    var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+        .WithTopicFilter(
+            f => { f.WithTopic("samples/temperature/living_room"); })
+        .Build();
+
+    await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+    Console.WriteLine("MQTT client subscribed to topic.");
+
+    Console.WriteLine("Press enter to exit.");
+    Console.ReadLine();
+}
+
+
+async Task SubscribeFile()
+{
+    var mqttFactory = new MqttFactory();
+
+    using var mqttClient = mqttFactory.CreateMqttClient();
+    var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
+
+    mqttClient.ApplicationMessageReceivedAsync += e =>
+    {
+        Console.WriteLine("Received application message.");
+        var payload = e.ApplicationMessage.Payload;
+        
+        // save file
+        
+        var path = "/Volumes/External/www/mqtt-dotnet/mqtt-dotnet-subscribe/assets/tkpp-cli.docx";
+        
+        File.WriteAllBytes(path, payload);
+        Console.WriteLine($"File saved to {path}");
+        return Task.CompletedTask;
+    };
+
+    await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+    var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+        .WithTopicFilter(
+            f => { f.WithTopic("samples/temperature/living_room/file"); })
+        .Build();
+
+    await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+    Console.WriteLine("MQTT client subscribed to topic.");
+
+    Console.WriteLine("Press enter to exit.");
+    Console.ReadLine();
+}
